e4ad5551af6457238945566c13dd73267858133f
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-12-21 23:13:20 +0100

    inits repair action

--- client/version_control/plugins/publish/validate_workspace_is_clean.py
+++ client/version_control/plugins/publish/validate_workspace_is_clean.py
@@ -1,11 +1,14 @@
 import os
 
 import pyblish.api
-from ayon_core.pipeline.publish import ValidateContentsOrder
-from ayon_core.pipeline import PublishXmlValidationError
+
+from ayon_core.pipeline.publish import (
+    ValidateContentsOrder,
+    RepairAction,
+    PublishValidationError,
+)
 
 from version_control.rest.perforce.rest_stub import PerforceRestStub
-
 
 class ValidateWorkspaceIsClean(pyblish.api.InstancePlugin):
     """Validates if local workspace has no uncomitted changes."""
@@ -14,9 +17,17 @@ class ValidateWorkspaceIsClean(pyblish.api.InstancePlugin):
     label = "Validate P4 workspace is clean"
     families = ["changelist_metadata"]
     targets = ["local"]
+    actions = [RepairAction]
 
     def process(self, instance):
         uncommitted_changes = PerforceRestStub.get_uncommitted_changes()
         if uncommitted_changes:
-            raise Exception("Workspace has uncommitted changes! Please commit or revert before publish.")
+            for change in uncommitted_changes:
+                self.log.error(f"Uncommitted change: {change}")
+            raise PublishValidationError("Workspace has uncommitted changes! Please commit or revert before publish.")
         # # TODO: check for stream updates
+
+    @classmethod
+    def repair(cls, instance):
+        # TODO: present little dialog to user to commit or revert
+        pass
